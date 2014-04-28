6306ef91698ebcba420ddc7e04a45763186a1f3d
Alex Gaynor <alex.gaynor@gmail.com>
2014-04-27 22:08:34 -0700

    Do some math in place

--- m.go
+++ m.go
@@ -15,15 +15,15 @@ func MontgomeryLadderExp(a, b, c *big.Int) *big.Int {
     a2 := new(big.Int).Mul(a, a)
     for pos := b.BitLen() - 1; pos >= 0; pos -- {
         if b.Bit(pos) == 0 {
-            a2 = new(big.Int).Mul(a2, a1)
-            a1 = new(big.Int).Mul(a1, a1)
+            a2.Mul(a2, a1)
+            a1.Mul(a1, a1)
         } else {
-            a1 = new(big.Int).Mul(a1, a2)
-            a2 = new(big.Int).Mul(a2, a2)
+            a1.Mul(a1, a2)
+            a2.Mul(a2, a2)
         }
 
-        a1 = new(big.Int).Mod(a1, c)
-        a2 = new(big.Int).Mod(a2, c)
+        a1.Mod(a1, c)
+        a2.Mod(a2, c)
     }
     return a1
 }
