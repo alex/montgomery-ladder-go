3bac968e5e7c67360c0f987545c6f3b0ae61e45e
Alex Gaynor <alex.gaynor@gmail.com>
2014-04-27 21:25:52 -0700

    Delete a lot of code by using the builtin bit stuff

--- m.go
+++ m.go
@@ -12,33 +12,17 @@ func MontgomeryLadderExp(a, b, c *big.Int) *big.Int {
     }
     a1 := a
     a2 := new(big.Int).Mul(a, a)
-    bits := b.Bits()
-    for pos := len(bits) - 1; pos >= 0; pos-- {
-        b := bits[pos]
-        // TODO: how to get the actual bit width of Word on this platform?
-        i := 7
-        // On the first element, we need to skip any leading 0s.
-        if pos == len(bits) - 1 {
-            for ; i >= 0; i-- {
-                if (b & (1 << uint(i))) != 0 {
-                    break
-                }
-            }
-            // Also skip the first bit.
-            i--
+    for pos := b.BitLen() - 1; pos >= 0; pos -- {
+        if b.Bit(pos) == 0 {
+            a2 = new(big.Int).Mul(a2, a1)
+            a1 = new(big.Int).Mul(a1, a1)
+        } else {
+            a1 = new(big.Int).Mul(a1, a2)
+            a2 = new(big.Int).Mul(a2, a2)
         }
-        for ; i >= 0; i-- {
-            if (b & (1 << uint(i))) == 0 {
-                a2 = new(big.Int).Mul(a2, a1)
-                a1 = new(big.Int).Mul(a1, a1)
-            } else {
-                a1 = new(big.Int).Mul(a1, a2)
-                a2 = new(big.Int).Mul(a2, a2)
-            }
 
-            a1 = new(big.Int).Mod(a1, c)
-            a2 = new(big.Int).Mod(a2, c)
-        }
+        a1 = new(big.Int).Mod(a1, c)
+        a2 = new(big.Int).Mod(a2, c)
     }
     return a1
 }
