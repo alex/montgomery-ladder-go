3a2ec6afe36fd133831cc8a6f5245d1233e6eae7
Alex Gaynor <alex.gaynor@gmail.com>
2014-04-27 22:15:11 -0700

    go fmt

--- m.go
+++ m.go
@@ -1,31 +1,31 @@
 package snakes
 
 import (
-    "math/big"
+	"math/big"
 )
 
 // Compute `a ** b % c`, just like `big.Int.Exp`, but do it in a way that is
 // more constant time. This is useful for crypto stuff.
 func MontgomeryLadderExp(a, b, c *big.Int) *big.Int {
-    if a.Sign() == 0 && b.Sign() == 0 {
-        return big.NewInt(1)
-    }
-    // For now we can't do the operations below in place, because we can't
-    // mutate `a1`, since we don't "own" it. If we can make `a1` a copy of `a`
-    // then we can do the below operations in place.
-    a1 := a
-    a2 := new(big.Int).Mul(a, a)
-    for pos := b.BitLen() - 1; pos >= 0; pos -- {
-        if b.Bit(pos) == 0 {
-            a2 = new(big.Int).Mul(a2, a1)
-            a1 = new(big.Int).Mul(a1, a1)
-        } else {
-            a1 = new(big.Int).Mul(a1, a2)
-            a2 = new(big.Int).Mul(a2, a2)
-        }
+	if a.Sign() == 0 && b.Sign() == 0 {
+		return big.NewInt(1)
+	}
+	// For now we can't do the operations below in place, because we can't
+	// mutate `a1`, since we don't "own" it. If we can make `a1` a copy of `a`
+	// then we can do the below operations in place.
+	a1 := a
+	a2 := new(big.Int).Mul(a, a)
+	for pos := b.BitLen() - 1; pos >= 0; pos-- {
+		if b.Bit(pos) == 0 {
+			a2 = new(big.Int).Mul(a2, a1)
+			a1 = new(big.Int).Mul(a1, a1)
+		} else {
+			a1 = new(big.Int).Mul(a1, a2)
+			a2 = new(big.Int).Mul(a2, a2)
+		}
 
-        a1.Mod(a1, c)
-        a2.Mod(a2, c)
-    }
-    return a1
+		a1.Mod(a1, c)
+		a2.Mod(a2, c)
+	}
+	return a1
 }
--- m_test.go
+++ m_test.go
@@ -1,32 +1,31 @@
 package snakes
 
 import (
-    "math/big"
-    "testing"
+	"math/big"
+	"testing"
 )
 
-
 func check(t *testing.T, a, b, c int) {
-    big_a := big.NewInt(int64(a))
-    big_b := big.NewInt(int64(b))
-    big_c := big.NewInt(int64(c))
-    z := new(big.Int).Exp(big_a, big_b, big_c)
-    got := MontgomeryLadderExp(big_a, big_b, big_c)
-    if (got.Cmp(z) != 0) {
-        t.Errorf("pow(%d, %d, %d) Expected: %s, got %s", a, b, c, z.String(), got.String())
-    }
+	big_a := big.NewInt(int64(a))
+	big_b := big.NewInt(int64(b))
+	big_c := big.NewInt(int64(c))
+	z := new(big.Int).Exp(big_a, big_b, big_c)
+	got := MontgomeryLadderExp(big_a, big_b, big_c)
+	if got.Cmp(z) != 0 {
+		t.Errorf("pow(%d, %d, %d) Expected: %s, got %s", a, b, c, z.String(), got.String())
+	}
 }
 
 func TestMontgomeryLadderModExp(t *testing.T) {
-    check(t, 0, 0, 1)
-    check(t, 0, 1, 1)
-    check(t, 1, 0, 1)
-    check(t, 2, 10, 1)
-    check(t, 4, 13, 1)
-    check(t, 13, 4, 1)
-    check(t, 2, 10, 15)
-    check(t, 3, 10, 15)
-    check(t, 13, 4, 7)
-    check(t, 19, 65, 3)
-    check(t, 1432, 432, 123)
+	check(t, 0, 0, 1)
+	check(t, 0, 1, 1)
+	check(t, 1, 0, 1)
+	check(t, 2, 10, 1)
+	check(t, 4, 13, 1)
+	check(t, 13, 4, 1)
+	check(t, 2, 10, 15)
+	check(t, 3, 10, 15)
+	check(t, 13, 4, 7)
+	check(t, 19, 65, 3)
+	check(t, 1432, 432, 123)
 }
