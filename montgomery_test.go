2941b8562bb672ddd7b922b610ea25a6defcc72c
Alex Gaynor <alex.gaynor@gmail.com>
2014-04-28 10:41:24 -0700

    Failing test

--- montgomery_test.go
+++ montgomery_test.go
@@ -5,31 +5,40 @@ import (
 	"testing"
 )
 
-func check(t *testing.T, a, b, c int) {
+func checkExpInt(t *testing.T, a, b, c int) {
 	bigA := big.NewInt(int64(a))
 	bigB := big.NewInt(int64(b))
 	bigC := big.NewInt(int64(c))
-	z := new(big.Int).Exp(bigA, bigB, bigC)
-	got := MontgomeryLadderExp(bigA, bigB, bigC)
+	checkExp(t, bigA, bigB, bigC)
+}
+
+func checkExp(t *testing.T, a, b, c *big.Int) {
+	z := new(big.Int).Exp(a, b, c)
+	got := MontgomeryLadderExp(a, b, c)
 	if got.Cmp(z) != 0 {
-		t.Errorf("pow(%d, %d, %d) Expected: %s, got %s", a, b, c, z.String(), got.String())
+		t.Errorf("pow(%s, %s, %s) Expected: %s, got %s", a, b, c, z, got)
 	}
 }
 
 func TestMontgomeryLadderExp(t *testing.T) {
-	check(t, 0, 0, 1)
-	check(t, 0, 1, 1)
-	check(t, 0, 2, 1)
-	check(t, 1, 0, 1)
-	check(t, 2, 0, 1)
-	check(t, 2, 10, 1)
-	check(t, 4, 13, 1)
-	check(t, 13, 4, 1)
-	check(t, 2, 10, 15)
-	check(t, 3, 10, 15)
-	check(t, 13, 4, 7)
-	check(t, 19, 65, 3)
-	check(t, 1432, 432, 123)
+	checkExpInt(t, 0, 0, 1)
+	checkExpInt(t, 0, 1, 1)
+	checkExpInt(t, 0, 2, 1)
+	checkExpInt(t, 1, 0, 1)
+	checkExpInt(t, 2, 0, 1)
+	checkExpInt(t, 2, 10, 1)
+	checkExpInt(t, 4, 13, 1)
+	checkExpInt(t, 13, 4, 1)
+	checkExpInt(t, 2, 10, 15)
+	checkExpInt(t, 3, 10, 15)
+	checkExpInt(t, 13, 4, 7)
+	checkExpInt(t, 19, 65, 3)
+	checkExpInt(t, 1432, 432, 123)
+	checkExpInt(t, 1, 65537, 2)
+	a, _ := new(big.Int).SetString("2938462938472983472983659726349017249287491026512746239764525612965293865296239471239874193284792387498274256129746192347", 0)
+	b, _ := new(big.Int).SetString("298472983472983471903246121093472394872319615612417471234712061", 0)
+	c, _ := new(big.Int).SetString("29834729834729834729347290846729561262544958723956495615629569234729836259263598127342374289365912465901365498236492183464", 0)
+	checkExp(t, a, b, c)
 }
 
 func BenchmarkBigIntExp(b *testing.B) {
