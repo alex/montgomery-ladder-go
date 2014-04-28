f224458d87164ed36e5980242b03aa344c894eae
Alex Gaynor <alex.gaynor@gmail.com>
2014-04-28 10:06:10 -0700

    Added a benchmark, is it good who knows?

--- montgomery_test.go
+++ montgomery_test.go
@@ -16,7 +16,7 @@ func check(t *testing.T, a, b, c int) {
 	}
 }
 
-func TestMontgomeryLadderModExp(t *testing.T) {
+func TestMontgomeryLadderExp(t *testing.T) {
 	check(t, 0, 0, 1)
 	check(t, 0, 1, 1)
 	check(t, 0, 2, 1)
@@ -30,4 +30,26 @@ func TestMontgomeryLadderModExp(t *testing.T) {
 	check(t, 13, 4, 7)
 	check(t, 19, 65, 3)
 	check(t, 1432, 432, 123)
+}
+
+func BenchmarkBigIntExp(b *testing.B) {
+	x := new(big.Int).Lsh(big.NewInt(1), 1374)
+	y := new(big.Int).Lsh(big.NewInt(1), 4096)
+	z := new(big.Int).Lsh(big.NewInt(1), 234)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		new(big.Int).Exp(x, y, z)
+	}
+}
+
+func BenchmarkMontgomeryLadderExp(b *testing.B) {
+	x := new(big.Int).Lsh(big.NewInt(1), 1374)
+	y := new(big.Int).Lsh(big.NewInt(1), 4096)
+	z := new(big.Int).Lsh(big.NewInt(1), 234)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		MontgomeryLadderExp(x, y, z)
+	}
 }
