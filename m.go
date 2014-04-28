c398d0d1a08f01ead71f97cbc27841985533a692
Alex Gaynor <alex.gaynor@gmail.com>
2014-04-27 22:09:39 -0700

    Simplify

--- m.go
+++ m.go
@@ -7,8 +7,7 @@ import (
 // Compute a ** b % c, just like big.Int.Exp, but do it in a way that is more
 // constant time. This is useful for crypto stuff.
 func MontgomeryLadderExp(a, b, c *big.Int) *big.Int {
-    zero := big.NewInt(0)
-    if a.Cmp(zero) == 0 && b.Cmp(zero) == 0 {
+    if a.Sign() == 0 && b.Sign() == 0 {
         return big.NewInt(1)
     }
     a1 := a
