f5303be4b8488678fc3b383bbff0d908532c79cd
Alex Gaynor <alex.gaynor@gmail.com>
2014-04-28 08:00:27 -0700

    Errors should not pass silently

--- montgomery.go
+++ montgomery.go
@@ -8,6 +8,9 @@ import (
 // Compute `a ** b % c`, just like `big.Int.Exp`, but do it in a way that is
 // more constant time. This is useful for crypto stuff.
 func MontgomeryLadderExp(a, b, c *big.Int) *big.Int {
+	if a.Sign() == -1 || b.Sign() == -1 || c.Sign() == -1 {
+		panic("MontgomeryLadderExp called with negative parameter.")
+	}
 	if b.Sign() == 0 {
 		return big.NewInt(1)
 	}
